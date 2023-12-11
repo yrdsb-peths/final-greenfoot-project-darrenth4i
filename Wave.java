9ef0ca4155e82b1ecc24fd9020d86e761f091c58
darrenth4i <150176044+darrenth4i@users.noreply.github.com>
2023-12-11 13:30:09 -0500

    Added commenting for wave class

--- Wave.java
+++ Wave.java
@@ -18,23 +18,30 @@ public class Wave extends Actor
         click();
     }
     
+    //Array for the wave images
     GreenfootImage[] waveAnimation = new GreenfootImage[3];
     public Wave(){
-        //Construct an array of 4 images of spike
+        //Construct an array of 3 images of wave
         for(int i = 0; i < waveAnimation.length; i++){
             waveAnimation[i] = new GreenfootImage("images/wave_idle/wave" + i  + ".png");
             waveAnimation[i].scale(waveAnimation[i].getWidth() / 4, waveAnimation[i].getHeight() / 4);
-            setImage(waveAnimation[1]);
+            setImage(waveAnimation[1]); //Default wave icon image
         }
     }
     
+    /**
+     * Method to change the image and location of the wave icon based on
+     * the Y-coordinate and if mouse is being pressed
+     */
     public void click(){
         MyWorld world = new MyWorld();
+        //When player is clicking, wave moves up
         if(Greenfoot.mousePressed(world)){
-            setImage(waveAnimation[2]);
-        }
+            setImage(waveAnimation[2]); //Wave icon up image
+        } 
+        //When player lets go, wave moves down
         else if(Greenfoot.mouseClicked(world)){
-            setImage(waveAnimation[1]);
+            setImage(waveAnimation[1]); //Wave icon down image
         }
     }
 }
