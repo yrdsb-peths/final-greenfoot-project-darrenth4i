8002ed44cc17db39807dad93ada24a67afb44086
darrenth4i <150176044+darrenth4i@users.noreply.github.com>
2024-01-08 13:03:28 -0500

    Created custom title screen background

--- TitleScreen.java
+++ TitleScreen.java
@@ -8,7 +8,6 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class TitleScreen extends World
 {
-    Label titleLabel = new Label("Wave Time", 60);
     /**
      * Constructor for objects of class TitleScreen.
      * 
@@ -18,7 +17,6 @@ public class TitleScreen extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
         
-        addObject(titleLabel, getWidth()/2, 70);
     }
     
     MyWorld gameWorld = new MyWorld();
