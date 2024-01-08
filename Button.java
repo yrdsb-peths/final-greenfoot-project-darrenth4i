96793db38e62cfd10cc04a52ab72b5b991d40171
darrenth4i <150176044+darrenth4i@users.noreply.github.com>
2024-01-08 13:25:29 -0500

    Add Button class and button images

---
+++ Button.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * An image that the user clicks to go to another World object
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Button extends Actor
+{
+    public Button(String imgPath){
+        setImage("images/button/"
+    }
+    
+    /**
+     * Act - do whatever the Button wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
