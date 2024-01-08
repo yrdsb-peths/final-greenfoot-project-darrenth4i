25e7c2529cae04f85cc99a1327a1ad7cdb823bc8
darrenth4i <150176044+darrenth4i@users.noreply.github.com>
2024-01-08 12:27:14 -0500

    Added title screen and transitions to game world with space or mouse click

---
+++ TitleScreen.java
@@ -0,0 +1,34 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Title screen to introduce player to the game
+ * 
+ * @author Darren
+ * @version 12/31/23
+ */
+public class TitleScreen extends World
+{
+    Label titleLabel = new Label("Wave Time", 60);
+    /**
+     * Constructor for objects of class TitleScreen.
+     * 
+     */
+    public TitleScreen()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        
+        addObject(titleLabel, getWidth()/2, 70);
+    }
+    
+    MyWorld gameWorld = new MyWorld();
+    /**
+     * Main world's act loop
+     */
+    public void act(){
+        //Start game once player presses space bar or clicks mouse
+        if(Greenfoot.isKeyDown("space") || Greenfoot.mousePressed(gameWorld)){
+            Greenfoot.setWorld(gameWorld);
+        }
+    }
+}
