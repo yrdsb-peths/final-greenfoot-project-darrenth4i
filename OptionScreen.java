fe01765a89fae56fcc7d05606e41748d53f211bb
darrenth4i <150176044+darrenth4i@users.noreply.github.com>
2024-01-12 12:25:16 -0500

    Resized images and added colour images as buttons in option screen

--- OptionScreen.java
+++ OptionScreen.java
@@ -20,5 +20,15 @@ public class OptionScreen extends World
         
         Button exit = new Button("exit", 8);
         addObject(exit, 25, 25);
+        
+        //Create buttons for changing colour of wave trail
+        Button blueTrail = new Button("blue", 2);
+        addObject(blueTrail, 130, 300);
+        
+        Button blackTrail = new Button("black", 2);
+        addObject(blackTrail, 195, 300);
+        
+        Button redTrail = new Button("red", 2);
+        addObject(redTrail, 260, 300);
     }
 }
