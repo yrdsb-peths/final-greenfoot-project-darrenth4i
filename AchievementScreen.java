b4a49c868f02c2c52cedfa68e81006dca676c701
darrenth4i <150176044+darrenth4i@users.noreply.github.com>
2024-01-17 21:42:31 -0500

    Achievement screen now displays new pictures based on certain point milestones

--- AchievementScreen.java
+++ AchievementScreen.java
@@ -26,10 +26,6 @@ public class AchievementScreen extends HelpScreen
     {
         super(col, ic, hiScore);
         
-        colour = col;
-        icon = ic;
-        highScore = hiScore;
-        
         //Create array with four images, each image showing new instructions
         for(int i = 0; i<2; i++){
             bg[i] = new GreenfootImage("images/achievement_screen/achievement" + i + ".png");
@@ -50,10 +46,45 @@ public class AchievementScreen extends HelpScreen
     
     public void act(){
         super.clicked(1, bg);
+        cheat();
+        checkScore();
     }
     
     public void checkScore(){
-        
+        if(super.current == 0){
+            if(super.highScore >= 50){
+                ach1.createImage("face0", 8);
+                ach2.createImage("face1", 9);
+                ach3.createImage("face2", 8);
+            }
+            else if(super.highScore >= 10){
+                ach1.createImage("face0", 8);
+                ach2.createImage("face1", 9);
+            }
+            else if(super.highScore >= 1){
+                ach1.createImage("face0", 8);
+            }
+        }
+        else{
+            if(super.highScore >= 312){
+                ach1.createImage("face3", 5);
+                ach2.createImage("face4", 11);
+                ach3.createImage("face5", 8);
+            }
+            else if(super.highScore >= 250){
+                ach1.createImage("face3", 5);
+                ach2.createImage("face4", 11);
+            }
+            else if(super.highScore >= 100){
+                ach1.createImage("face3", 5);
+            }
+        }
+    }
+    
+    public void cheat(){
+        if(Greenfoot.isKeyDown("o") && Greenfoot.isKeyDown("p")){
+            super.highScore = 1000;
+        }
     }
     
 }
